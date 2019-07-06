9067165e26b3070c0638f09743fe224ab011d452
Lorenzo Mussi <lorenzo.mussi@docomodigital.com>
2019-07-06 13:02:02 +0200

    started first saga

--- src/pages/Today/Today.tsx
+++ src/pages/Today/Today.tsx
@@ -1,29 +1,31 @@
 import React, {Component} from 'react';
 import {TodayContainer} from './Today.style';
 import {RouteProps} from 'react-router-dom';
-
 import MeteoCard from '../../components/MeteoCard/MeteoCard';
+import {connect} from 'react-redux'
 
 interface iProps {
     history: any,
-  }
+    requestWeather: Function
+}
   
-  type TodayProps = iProps & RouteProps;
-  
-  interface TodayState {
-  }
+type TodayProps = iProps & RouteProps;
 
-export default class Today<TodayProps, TodayState> extends Component {
+interface TodayState {
+}
+
+class Today extends Component<TodayProps, TodayState> {
     constructor(props: TodayProps) {
         super(props);
     }
 
     componentDidMount() {
-        window.fetch('api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=c164c4f116a450a01bbd21524e76502e')
-            .then(r => r.json())
-            .then((r) => {
-                console.log(r);
-        });
+        // window.fetch('https://api.openweathermap.org/data/2.5/weather?q=FLORENCE,it&APPID=c164c4f116a450a01bbd21524e76502e')
+        //     .then(r => r.json())
+        //     .then((r) => {
+        //         console.log(r);
+        // });
+        this.props.requestWeather({name: 'Florence', country: 'it-It'});
     }
 
     render(){
@@ -33,3 +35,14 @@ export default class Today<TodayProps, TodayState> extends Component {
         </TodayContainer>
     }
 }
+
+const mapStateToProps = (state: any) => ({
+  });
+  
+  const mapDispatchToProps = (dispatch: any) => ({
+      requestWeather: (city: {name: string, country: string}) => {
+          dispatch({ type: 'REQUEST_WEATHER', city})
+      }
+  });   
+  
+  export default connect(mapStateToProps, mapDispatchToProps)(Today);
--- src/saga/sagas/weatherSaga.tsx
+++ src/saga/sagas/weatherSaga.tsx
@@ -1,7 +1,20 @@
 import {takeLatest, take, put} from 'redux-saga/effects';
 
+const endpoint = 'https://api.openweathermap.org/data/2.5/weather?';
+const apikey = 'c164c4f116a450a01bbd21524e76502e';
+
 export function* watchForWeatherRequest() {
-    const test = yield takeLatest('REQUEST_WEATHER', function requestWeather(action) {
+    const test = yield takeLatest('REQUEST_WEATHER', function requestWeather(action: {
+        type: string,
+        city: {
+            name: string,
+            country: string
+        }}) {
         console.log(['SAGA', 'WEATHER_SAGA', 'REQUEST_WEATHER', action]);
+        const queryParams = `q=${action.city.name},${action.city.country}&appid=${apikey}`;
+
+        window.fetch(endpoint + queryParams).then(r => r.json()).then((resp) => {
+            console.log(resp);
+        })
     });
 }
